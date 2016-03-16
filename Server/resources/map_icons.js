960e55273a2f91ccd51cf720f1f9d1234da65884
Junior Gregoire <junior.gregoire@gmail.com>
2016-03-15 23:39:42 -0400

    Change bike icon

---
+++ Server/resources/map_icons.js
@@ -0,0 +1,12 @@
+var bikeIcon = L.icon({
+    iconUrl: 'resources/images/ic_directions_bike_grey600_24dp.png',
+    iconRetinaUrl: 'resources/images/ic_directions_bike_grey600_48dp.png',
+    iconSize: [48, 48],
+    
+    popupAnchor: [0, -20],
+    shadowUrl: 'resources/images/bike-shadow.png',
+    shadowRetinaUrl: 'resources/images/marker-shadow.png',
+    shadowSize: [90, 24],
+    shadowAnchor: [35, 5]
+    
+});
